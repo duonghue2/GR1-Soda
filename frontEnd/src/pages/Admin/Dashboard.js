e1d0dbd6c09c62ae15384243919d3a21bb5a781c
DESKTOP-9NGASCB\DELL <honghue2911999@gmail.com>
2021-06-25 22:53:59 +0700

    side admin update update+ edit product

--- frontEnd/src/pages/Admin/Dashboard.js
+++ frontEnd/src/pages/Admin/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Row, Popover, Table, Space, Input, message, Image, Button, Pagination } from 'antd';
+import { Menu, Row, Popover, Table, Space, Input, message, Image, Button, Pagination, Modal } from 'antd';
 import { DeleteOutlined, UserOutlined, EditOutlined, CheckOutlined, SaveOutlined } from '@ant-design/icons'
 import { reactLocalStorage } from 'reactjs-localstorage';
 import { server } from '../../enviroment'
@@ -20,14 +20,20 @@ const content = ({ data, logout }) => {
 }
 class Dashboard extends React.Component {
     state = {
-        current: "order",
+        current: "product",
         visible: false,
         products: [],
         orders: [],
         index: 0,
         isEdit: [],
         total: 0,
-        loading: false
+        loading: false,
+        selectedRows: [],
+        isSave: false,
+        currentPage: 1,
+        update: {},
+        selectedRowKeys: [],
+
 
     };
     logout = (e) => {
@@ -38,42 +44,59 @@ class Dashboard extends React.Component {
         window.location = "/";
     }
     handleClick = e => {
+        debugger;
+        this.state.current = e.key;
+        this.state.currentPage = 1;
+        this.state.selectedRows = [];
+        this.state.isSave = false;
+        this.state.index = 0;
+        this.state.loading = true;
 
-        this.setState({ current: e.key });
-        this.getData();
+        this.getData(1)
     };
-    getData = async () => {
+    getData = async (x) => {
+        this.state.isSave = false;
+        this.state.isEdit.forEach(element => {
+            element = false;
+        })
+        this.state.selectedRowKeys = [];
+        this.state.selectedRows = []
+        debugger
         let token = reactLocalStorage.get("token");
         let userId = reactLocalStorage.getObject("userInfo").userId;
         if (userId) {
-            this.state.index = this.state.index + 1;
+            this.state.currentPage = x;
             let payload = {
                 token, userId,
-                pageSize: 20,
-                currentPage: this.state.index,
+                pageSize: 9,
+                currentPage: x,
                 state: 'pending'
             }
-            // if (this.state.current == 'product') axios.post(server + 'api/ProductDetails/admin', payload).then(resp => {
-            //     console.log(resp)
-            //     this.state.products = resp.data.data != null ? resp.data.data : [];
-            //     this.state.total = resp.data.data != null ? resp.data.total : 0;
-            //     this.setState(this.state)
-            // }).catch(e => {
-            //     throw e;
-            // });
-            // else 
-            axios.post(server + 'api/Orders/list-order', payload).then(resp => {
+            if (this.state.current == 'product') axios.post(server + 'api/ProductDetails/admin', payload).then(resp => {
                 console.log(resp)
-                this.state.orders = resp.data.data != null ? resp.data.data : [];
+                this.state.loading = false;
+                this.state.products = resp.data.data != null ? resp.data.data : [];
+                this.state.products = this.state.products.map((item, index) => ({ key: index, ...item }));
                 this.state.total = resp.data.data != null ? resp.data.total : 0;
                 this.setState(this.state)
             }).catch(e => {
                 throw e;
             });
+            else
+                axios.post(server + 'api/Orders/list-order', payload).then(resp => {
+                    console.log(resp)
+                    this.state.loading = false;
+                    this.state.orders = resp.data.data != null ? resp.data.data : [];
+                    this.state.orders = this.state.orders.map((item, index) => ({ key: index, ...item }));
+                    this.state.total = resp.data.data != null ? resp.data.total : 0;
+                    this.setState(this.state)
+                }).catch(e => {
+                    throw e;
+                });
         }
     }
     componentDidMount() {
-        this.getData()
+        this.getData(1)
         // Array.apply(null, Array(3)).map(String.prototype.valueOf, "hi")
     }
 
@@ -85,14 +108,72 @@ class Dashboard extends React.Component {
     onSearch = (e) => {
         window.location = "/admin/search/" + e;
     }
-    delete = () => { }
-    submit = () => { }
-    edit = (record) => {
-        this.state.isEdit[record] = true;
+    delete = (e) => {
+        e.preventDefault();
+        let token = reactLocalStorage.get("token");
+        let userId = reactLocalStorage.getObject("userInfo").userId;
+        this.state.loading = true;
+
+
+        this.state.selectedRows.forEach(element => {
+            let payload = {
+                token, userId,
+                id: element.productDetailId
+            }
+            axios.post(server + 'api/ProductDetails/admin/delete', payload).then(resp => {
+                if (resp.data.data) {
+                    this.getData(this.state.currentPage);
+                }
+            }).catch(e => {
+                throw e;
+            });
+        });
+
+    }
+    submit = (e) => {
+        e.preventDefault();
+    }
+    edit = (e) => {
+        e.preventDefault();
+
+        let key = this.state.selectedRows[0].key;
+        this.state.isSave = true;
+        this.state.isEdit[key] = true;
+
         this.setState(this.state);
     }
-    update = () => {
+    update = (e) => {
+        e.preventDefault();
+        debugger;
+        let token = reactLocalStorage.get("token");
+        let userId = reactLocalStorage.getObject("userInfo").userId;
+        this.state.update.token = token;
+        this.state.update.userId = userId
+        if (this.state.current == 'product') axios.post(server + 'api/ProductDetails/admin/update', this.state.update).then(resp => {
+            debugger
+            if (resp.data.data) {
+                window.location.reload()
 
+                // this.getData(this.state.currentPage);
+
+            }
+
+        }).catch(e => {
+            throw e;
+        });
+    }
+    handleChange(record, e) {
+        debugger
+        let update = record;
+        update[e.target.name] = e.target.value;
+        this.setState({ update: update })
+        console.log(update)
+
+    }
+    showModal = (e) => {
+        e.preventDefault()
+        this.state.visible = !this.state.visible;
+        this.setState(this.state);
     }
     render() {
         let userInfo = reactLocalStorage.get("token");
@@ -105,7 +186,8 @@ class Dashboard extends React.Component {
                 sorter: (a, b) => a.name.length - b.name.length,
 
                 ellipsis: true,
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+
+                render: (text, record, index) => <Input defaultValue={text} name="name" onChange={e => this.handleChange(record, e)} disabled={!this.state.isEdit[index]} />
             },
             {
                 title: 'Price',
@@ -114,14 +196,14 @@ class Dashboard extends React.Component {
                 sorter: (a, b) => a.originPrice - b.originPrice,
 
                 ellipsis: true,
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+                render: (text, record, index) => <Input value={text} disabled={!this.state.isEdit[index]} />
             }, {
                 title: 'Sale price',
                 dataIndex: 'salePrice',
                 key: 'salePrice',
                 sorter: (a, b) => a.salePrice - b.salePrice,
 
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+                render: (text, record, index) => <Input value={text} disabled={!this.state.isEdit[index]} />
             },
             {
                 title: 'Size',
@@ -131,27 +213,17 @@ class Dashboard extends React.Component {
             {
                 title: 'Quantity',
                 dataIndex: 'quantity',
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+                render: (text, record, index) => <Input value={text} disabled={!this.state.isEdit[index]} />
             }, {
                 title: 'Category',
                 dataIndex: 'category',
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+                render: (text, record, index) => <Input value={text} disabled={!this.state.isEdit[index]} />
             },
             {
                 title: 'SubCategory',
                 dataIndex: 'subCategory',
-                render: (text, record) => <input value={text} disabled={!this.state.isEdit[record]} />
+                render: (text, record, index) => <Input value={text} disabled={!this.state.isEdit[index]} />
 
-            }, {
-                title: "Action",
-                key: "action",
-                render: record => (
-                    <Space size="middle">
-                        {this.state.isEdit && <SaveOutlined onClick={this.update(record)} />}
-                        <DeleteOutlined onClick={record => this.delete(record)} />
-                        <EditOutlined onClick={record => this.edit(record)} />
-                    </Space>
-                )
             }
         ];
         const columnOrder = [
@@ -185,18 +257,15 @@ class Dashboard extends React.Component {
                 title: 'State',
                 dataIndex: 'state',
                 key: 'state',
-            },
-            {
-                title: "Action",
-                key: "action",
-                render: record => (
-                    <Space size="middle">
-                        <DeleteOutlined onClick={() => this.delete(record)} />
-                        <CheckOutlined onClick={() => this.submit(record)} />
-                    </Space>
-                )
             }
         ];
+        const rowSelection = {
+            selectedRowKeys: this.state.selectedRowKeys,
+            onChange: (selectedRowKeys, selectedRows) => {
+                this.setState({ selectedRows, selectedRowKeys })
+
+            }
+        };
 
         // if (userInfo.isAdmin)
         return (
@@ -229,15 +298,45 @@ class Dashboard extends React.Component {
                         </Popover>
                     </div>
                 </Row>
-                <Row justify="space-around" align="middle">
-                    {this.state.current == 'product' && <Button> Create product</Button>}
-
+                <Row justify="start" align="middle" style={{ marginBottom: '15px' }}>
+                    {this.state.current == 'product' && <Button onClick={e => this.showModal(e)}> Create product</Button>}
+                    {this.state.selectedRows.length == 1 && this.state.current == 'product' && !this.state.isSave && <Button onClick={e => this.edit(e)}>  <EditOutlined />Edit</Button>}
+                    {this.state.selectedRows.length == 1 && this.state.current == 'product' && this.state.isSave && <Button onClick={e => this.update(e)}>  <EditOutlined />Save</Button>}
+                    {this.state.selectedRows.length > 0 && <Button onClick={e => this.delete(e)} >  <DeleteOutlined />Delete</Button>}
+                    {this.state.selectedRows.length > 0 && this.state.current == 'order' && <Button onClick={e => this.submit(e)} >   <CheckOutlined />Approve</Button>}
                 </Row>
-                <Row><Pagination defaultCurrent={this.state.index} total={this.state.total} pageSize={20} onChange={this.getData} /></Row>
-                <Table columns={this.state.current == 'product' ? columnProduct : columnOrder} dataSource={this.state.current == 'product' ? this.state.products : this.state.orders}
+                <Row style={{ marginBottom: '15px' }}><Pagination defaultCurrent={1} current={this.state.currentPage} total={this.state.total} pageSize={9} onChange={(x) => this.getData(x)} /></Row>
+                {this.state.current == 'product' && <Table columns={columnProduct} dataSource={this.state.products}
 
+                    rowSelection={{ ...rowSelection }}
                     loading={this.state.loading}
-                />
+
+                />}
+                {this.state.current == 'order' && <Table columns={columnOrder} dataSource={this.state.orders}
+                    rowSelection={{ ...rowSelection }}
+                    loading={this.state.loading}
+                    onRow={(record, rowIndex) => {
+                        return {
+                            onClick: event => {
+                                debugger;
+                                this.edit(rowIndex);
+
+                            }
+                        }
+                    }}
+                />}
+                <Modal
+                    title="Modal 1000px width"
+                    centered
+                    visible={this.state.visible}
+                    onOk={(e) => this.submit(e)}
+                    onCancel={(e) => this.showModal(e)}
+                    width={1000}
+                >
+                    <p>some contents...</p>
+                    <p>some contents...</p>
+                    <p>some contents...</p>
+                </Modal>
 
             </div >
 
